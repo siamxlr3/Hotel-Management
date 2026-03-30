64582c128875951f6c9bf49c388620f0f5c68575
siamxlr3 <siamxlr3@gmail.com>
2026-03-31 03:47:32 +0600

    Revert to native PHP environment and add automatic storage linking in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Automatically create storage link if it's missing (helps with Render/Docker)
+        if (!file_exists(public_path('storage'))) {
+            try {
+                \Illuminate\Support\Facades\Artisan::call('storage:link');
+            } catch (\Exception $e) {
+                // Ignore errors if link cannot be created (e.g. read-only filesystem)
+            }
+        }
     }
 }
